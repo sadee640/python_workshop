431992df46ae805719a028f877298377ba9df60c
unknown <229082@TECH.WYB.AC.LK>
2025-03-26 20:21:58 +0530

    Your descriptive commit message

--- task1.py
+++ task1.py
@@ -1,9 +1,8 @@
-numbers = []
+highest = int(input("Enter number 1: "))
 
-for i in range(5):
-    num = int(input(f"Enter number {i + 1}: "))
-    numbers.append(num)
+for i in range(2, 6):
+    num = int(input(f"Enter number {i}: "))
+    if num > highest:
+        highest = num
 
-highest_value = max(numbers)
-
-print("The highest value is:", highest_value)
+print("The highest value is:", highest)
