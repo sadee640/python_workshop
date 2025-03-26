aedf022a7fe3e84917cbb52336181009a49e4359
unknown <229082@TECH.WYB.AC.LK>
2025-03-26 20:07:51 +0530

    Your descriptive commit message

--- ABc.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-
-print("kdnm jayasanka \n wayamba university of srilanka \n229082.")
-=======
-
-print("kdnm jayasanka \n wayamba university of srilanka \n229082")
->>>>>>> 63d93123fa03944fac94627afcb80313653e9519
